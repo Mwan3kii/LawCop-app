e359aeb10946c1635aae332b076ee7104a99e98f
Mwan3kii <agathawanja303@gmail.com>
2024-06-21 21:40:26 +0300

    reports

---
+++ web_dynamic/static/scripts/report.js
@@ -0,0 +1,36 @@
+document.getElementById('reporForm').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const county = document.getElementById('county').value;
+    const location = document.getElementById('location').value;
+    const crimeType = document.getElementById('crime').value;
+    const datetime = document.getElementById('datetime').value;
+    const description = document.getElementById('description').value;
+
+    const reportData = {
+        county,
+        location,
+        crimeType,
+        datetime,
+        description,
+    };
+    try {
+        const response = await fetch('/api/reports', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(reportData),
+        });
+
+        if (response.ok) {
+            alert('Report submitted successfully');
+            window.location.href = '/alert.html';
+        } else {
+            const errorData = await response.json();
+            alert('Error: ' + errorData.message);
+        }
+    } catch (error) {
+        console.error('Error submitting report:', error);
+        alert('Error submitting report. Please try again later.');
+    }
+});
